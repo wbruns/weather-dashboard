530018efcd4eae4599db3a7dd3b60c4456c81cd1
wbruns <wbruns07@gmail.com>
2022-02-06 15:08:24 -0600

    added: cities obj for storing, started loadCities function, daily forecast info now displays properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 // my open weather api key
 var apiKey = '4d78acdcd141fb66bc220ffe67b06fc4';
-
-
-https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
+// obj for storage
+var cities = {};
+console.log(cities);
 
 // code wont run till dom is loaded
 $(document).ready(function() {
@@ -25,7 +24,7 @@ $(document).ready(function() {
         console.log(lat);
         console.log(openData);
         // call the one call api
-        var oneUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&exclude=minutely,hourly,alerts&appid=' + apiKey;
+        var oneUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' + lon + '&units=imperial&exclude=minutely,hourly,alerts&appid=' + apiKey;
         fetch(oneUrl).then(function(response) {
           response.json().then(function(oneData) {
             console.log(oneData);
@@ -41,6 +40,41 @@ $(document).ready(function() {
     $('#current-wind').html(oneData.current.wind_speed);
     $('#current-humidity').html(oneData.current.humidity);
     $('#current-uv').html(oneData.current.uvi);
+    // put the data in our cities object to save
+    var current = {
+      temp: oneData.current.temp,
+      wind_speed: oneData.current.wind_speed,
+      humidity: oneData.current.humidity,
+      uvi: oneData.current.uvi
+    };
+    $.extend(cities, current);
+    console.log(current);
+    console.log(cities);
+    // array for daily forecast cards
+    var dailyArray = $(".card-deck .card").toArray();
+    // loop over and display info
+    $.each(dailyArray, function(i) {
+      $(this).find('.daily-temp').html(oneData.daily[i].temp.day);
+      $(this).find('.daily-wind').html(oneData.daily[i].wind_speed);
+      $(this).find('.daily-humidity').html(oneData.daily[i].humidity);
+      $(this).find('.daily-uv').html(oneData.daily[i].uvi);
+    })
+  };
+  // function for loading cities object from storage
+  var loadCities = function() {
+    cities = JSON.parse(localStorage.getItem('cities'));
+    // if nothing in localStorage, recreate cities object with arrays for forecast time
+    if (!cities) {
+      cities = {
+        current: {},
+        daily: {}
+      };
+    }
+    // loop over object
+    // $.each(cities, function(list,arr) {
+    //   // loop over sub-array
+    //   arr.forEach(function())
+    // })
   };
   
   // city search button is clicked
